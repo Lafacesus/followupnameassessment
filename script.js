735ceb558b2e2572aa30544a313456a47a772c1d
Lafacesus <jamaicanflower30000@gmail.com>
2022-08-25 22:35:42 -0700

    Commit changes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 alert('I am not a robot')
+class Person {
+    constructor(firstName,lastName,age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+}
+let Lafacesus;
+document.getElementById("submit").addEventListener("click", function() {
+    document.getElementById("message").innerHTML = "Hello World";
+    const firstName = document.getElementById("firstName").value;
+    Lafacesus = new Person(firstName,lastName,age)
+  });
